5f1f25637bcd7f44a89deac50d9a37bae7cc7372
gemox94 <gerardo.mx.rz@gmail.com>
2018-07-15 21:37:17 -0500

    Added route for testing user token

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,11 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::middleware('auth:api')->get('test-token/user', function(Request $request) {
+    $user = $request->user();
+    return response()->json(['user' => $user]);
+});
+
 /*
  *  Authorization endpoints
  */
